5619d2913b7f722a0eefd8b10ba4cf3c58e54a5a
Regev Golan <look4regev@gmail.com>
2024-11-07 13:04:03 +0200

    Init main code

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+llm.invoke("Hello, world!")
